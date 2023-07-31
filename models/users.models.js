383facaa08bee26640d559b12a9993daa7855651
Adil <adil.jawaid@outlook.com>
2023-07-31 16:39:32 +0100

    Attempting to retrieve data from database

--- models/users.models.js
+++ models/users.models.js
@@ -1,11 +1,11 @@
 const db = require("../connection.js");
 const mongoose = require("mongoose");
 const usersSchema = new mongoose.Schema({
-  _id: { type: Object, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   age: { type: Number, required: true },
 });
+
 const user = mongoose.model("user", usersSchema);
 
 const selectAllUsers = () => {
