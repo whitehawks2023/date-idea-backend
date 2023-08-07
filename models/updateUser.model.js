c78d5cb2ee1a81b1afafb6a27865c3e7d1f3d1e9
Olivia Motevalli <olivia.motevalli.aliabadi@gmail.com>
2023-08-07 01:20:08 +0100

    patch password updated model

--- models/updateUser.model.js
+++ models/updateUser.model.js
@@ -1,24 +1,44 @@
+// ORIGINAL
+// const { user } = require("./selectUsers.model");
+
+// const updateUser = (username, first_name, last_name, email, avatar) => {
+//   const detailsObj = {};
+
+//   if (first_name !== undefined) {
+//     detailsObj.first_name = first_name;
+//   }
+//   if (last_name !== undefined) {
+//     detailsObj.last_name = last_name;
+//   }
+//   if (email !== undefined) {
+//     detailsObj.email = email;
+//   }
+//   if (avatar !== undefined) {
+//     detailsObj.avatar = avatar;
+//   }
+
+//   return user
+//     .updateOne({ username: username }, detailsObj)
+//       .then((updatedUser) => {
+//       return updatedUser;
+//     });
+// };
+
+// module.exports = updateUser;
+
+// NEW UPDATE PASSWORD
 const { user } = require("./selectUsers.model");
 
-const updateUser = (username, first_name, last_name, email, avatar) => {
+const updateUser = (password) => {
   const detailsObj = {};
 
-  if (first_name !== undefined) {
-    detailsObj.first_name = first_name;
-  }
-  if (last_name !== undefined) {
-    detailsObj.last_name = last_name;
-  }
-  if (email !== undefined) {
-    detailsObj.email = email;
-  }
-  if (avatar !== undefined) {
-    detailsObj.avatar = avatar;
+  if (password !== undefined) {
+    detailsObj.password = password;
   }
 
   return user
     .updateOne({ username: username }, detailsObj)
-      .then((updatedUser) => {
+    .then((updatedUser) => {
       return updatedUser;
     });
 };
